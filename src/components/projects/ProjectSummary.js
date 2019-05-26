8f447108e0ce2ff95c1d295e1961d44c884b046e
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-05-26 17:37:00 -0300

    project list ok

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProjectSummary = ({ project }) => (
   <div className="card z-depth-0 project-summary">
-    <div className="card-content grey-text text-darken-3">
-      <span className="card-title">{project.title}</span>
-      <p>{project.content}</p>
-      <p className="grey-text">Posted by Net Ninja</p>
-    </div>
+    <Link to={`/project/${project.id}`}>
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">{project.title}</span>
+        <p>{project.content}</p>
+        <p className="grey-text">Posted by Net Ninja</p>
+      </div>
+    </Link>
   </div>
 );
 

5400558f0d7e1fab4a7a6180c2c2ec7e824971cb
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-05-28 20:04:54 -0300

    projct summary ok

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProjectSummary = ({ project }) => (
+const ProjectSummary = ({ project }) => {
+  console.log('project:', project)
+  
+  return (
   <div className="card z-depth-0 project-summary">
     <Link to={`/project/${project.id}`}>
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
         <p>{project.content}</p>
-        <p className="grey-text">Posted by Net Ninja</p>
+        <p className="grey-text">Posted by {project.authFirstName}</p>
+        <p className="grey-text">{project.createdAt.seconds}</p>
+        
       </div>
     </Link>
   </div>
-);
+);}
 
 export default ProjectSummary;
